3359f0380f77d2d64f9c826c6fe2c7e394a32bb0
ViktoriyaTru <viktoriya.trubitsina@gmail.com>
2024-05-05 22:04:31 +0300

    Вывод нового массива

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
 ﻿// Вводим массив строк 
 
-System.Console.WriteLine("Введите строки через пробел:");
-string [] input = Console.ReadLine().Split(' ');
+Console.WriteLine("Введите строки через пробел:");
+string[] input = Console.ReadLine().Split(' ');
 
 // Формируем новый массив из полученых строк, длинной меньше или = 3 символа
 
 string[] result = new string[input.Length];
 int i = 0;
-for (i=0; i < input.Length; i++)
+for (i = 0; i < input.Length; i++)
 {
     if (input[i].Length <= 3)
     {
-        result[i] = input[i]
+        result[i] = input[i];
     }
+}
+
+// Вывод нового массива на экран
+
+Console.WriteLine("Строки длинной меньше или равной 3 символам:");
+for (int j = 0; j < result.Length; j++)
+{
+    Console.Write(result[j] + " ");
+
 }
