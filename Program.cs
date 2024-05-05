ac1efc189fca18c1ec8d509a192f4887afe0e11a
ViktoriyaTru <viktoriya.trubitsina@gmail.com>
2024-05-05 21:50:57 +0300

    Формируем новый массив

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Вводим массив строк 
+
+System.Console.WriteLine("Введите строки через пробел:");
+string [] input = Console.ReadLine().Split(' ');
+
+// Формируем новый массив из полученых строк, длинной меньше или = 3 символа
+
+string[] result = new string[input.Length];
+int i = 0;
+for (i=0; i < input.Length; i++)
+{
+    if (input[i].Length <= 3)
+    {
+        result[i] = input[i]
+    }
+}
